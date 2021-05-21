ef5082dd7e13212e9afaa3101ea7374bdf4f304c
hiwad rashad <hiwadrashad112@hotmail.com>
2021-05-21 10:18:36 +0200

    added comments

--- Reflection/Program.cs
+++ Reflection/Program.cs
@@ -10,22 +10,46 @@ namespace Reflection
     {
         public static void Main(string[] args)
         {
-            var item = System.Reflection.Assembly.LoadFile(@"E:\Google Chrome Downloads\ReflectThis.dll");
-            List<Type> Types = new List<Type>();
-            foreach (var indexitem in item.GetTypes())
+            Console.WriteLine("=============================== reflection assembly object metadata ======================================");
+            Console.WriteLine("\n");
+            Console.WriteLine("Load in dll file as assembly object at run time");
+            Console.WriteLine("\n");
+            var assembly = System.Reflection.Assembly.LoadFile(@"E:\Google Chrome Downloads\ReflectThis.dll");
+            Console.WriteLine("Get all types from assembly object");
+            var types = assembly.GetTypes();
+            Console.WriteLine("\n");
+            Console.WriteLine("write down type name and method parameter and return type if available");
+            Console.WriteLine("\n");
+            Console.WriteLine("==========================================================");
+            foreach (var item in types)
             {
-                Types.Add(indexitem);
+                Console.WriteLine($"Type name:  {item.FullName}");
+                if (item.GetMethods().Count() > 0)
+                {
+                    Console.WriteLine("\n");
+                    Console.WriteLine("Methods:");
+                    foreach (var item2 in item.GetMethods())
+                    {
+                       Console.WriteLine("\n");
+                       Console.WriteLine($"Return type {item2.ReturnType.FullName}");
+                       Console.WriteLine("\n");
+                    }
+                    Console.WriteLine("==========================================================");
+                    Console.WriteLine("\n");
+                }
             }
-            var item2 = item.GetTypes();
-            var item3 = item2.FirstOrDefault();
-            var item6 = item3.GetMethods().FirstOrDefault();
-            var item7 = item6.ReturnType;
-            var item4 = item3.GetMembers();
-            var item5 = item4.FirstOrDefault();
+            //var item3 = item2.FirstOrDefault();
+            //var item6 = item3.GetMethods().FirstOrDefault();
+            //var item7 = item6.ReturnType;
+            //var item4 = item3.GetMembers();
+            //var item5 = item4.FirstOrDefault();
 
-            var item435 = default(Type);
-
-            object instance = Activator.CreateInstance()
+            //var item435 = default(Type);
+            //foreach (var item item.GetTypes())
+            //{ 
+            
+            //}
+            //object instance = Activator.CreateInstance()
             
         }
     }
